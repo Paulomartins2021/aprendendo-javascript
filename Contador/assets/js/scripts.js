69db4fe46ace06269dc12e57ef978b64c195ff95
Paulomartins2021 <bmcomercialster@gmail.com>
2022-07-01 21:24:18 -0400

    atualizacao scripts

--- Contador/assets/js/scripts.js
+++ Contador/assets/js/scripts.js
@@ -2,7 +2,7 @@
 var currentNumberWrapper = document.getElementById("currentNumber")
 var currentNumber = 0;
 function increment(){
-	currentNumber = currentNumber +1 ;
+	currentNumber = currentNumber + 1 ;
 	currentNumberWrapper.innerHTML = currentNumber;
 }
 function increment(){
