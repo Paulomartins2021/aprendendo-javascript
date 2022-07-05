abaedd15ec5cba91086fc7e36f71051877850301
Paulomartins2021 <bmcomercialster@gmail.com>
2022-07-05 17:45:32 -0400

    atualizacao to-do-list script.js e styles.css

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -2,8 +2,31 @@ function addNovaTarefa(){
     const input =document.getElementById('nova-tarefa-input');
     const novaTarefaNome = input.value;
     const novaTarefaLi =document.createElement('li');
-    novaTarefaLi.innerHTML = novaTarefaNome;
+    const checkbox = document.createElement ('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('tarefa-checkbox');
+    ckeckbox.addEventListener('change', function(){
+        if( this.checked){
+            completarTarefa(this.parenteNode)
+        }
+        else {
+            reverterTarefa(this.parenteNode)
+        }
+    })
+    novaTarefaLi.appendChild(checkbox);
+    novaTarefaLi.innerHTML += novaTarefaNome;
     const listaTarefasIncompletas =document.getElementById('tarefas-incompletas');
     listaTarefasIncompletas.appendChild(novaTarefaLi);
 
+}
+function completarTarefa (tarefaLI){
+     tarefaLI.remove();
+     const listaTarefasCompletas = document.getElementById('tarefas-completas');
+     listaTarefasCompletas.appendChild(tarefaLI);
+
+}
+function reverterTarefa (tarefaLI){
+    tarefaLI.remove()
+    const listaTarefasIncompletas = document.getElementById('tarefas-incompletas')
+    listaTarefasIncompletas.appendChild(tarefaLI)
 }
