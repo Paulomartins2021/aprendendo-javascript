fbfdb61bdbeb028061728428c5831b43308bebcb
Paulomartins2021 <bmcomercialster@gmail.com>
2022-07-06 17:08:05 -0400

    aprofundando funcoes

---
+++ Danado javaScript sintaxe basica/aula 6 aprofundando funcoes/tiposDeFuncao.js
@@ -0,0 +1,33 @@
+// tipos de funcoes
+// declarativas
+
+function funcao(){
+    console.log("Sou uma mensagem de uma funcao declarativa")
+}
+funcao()
+
+// expressoes de funcoes sao funcoes atribuidas a expressoes. a nomeacao das funcoes por expressao e opcional ex1= var funcao - function nomeDaFuncao(){ ${ instrucao}} e ex2 = var funcao = function(){$ {instrucao};};
+
+// com nomeacao
+
+var funcao = function funcao(){
+    console.log ("sou uma mensagem da funcao de expressao")
+}
+funcao()
+
+// sem nomeacao
+
+var funcao = function(){
+    console.log ("sou uma mensagem da funcao de expressao")
+}
+funcao()
+
+// Arrow Function =>. sao funcoes de expressao de sintaxe curta. Arrow functions sempre sao anonimas, e portanto nao podem ser nomeadas, devem ser declarada com parenteses(). seguido de => e depois Chaves {}
+
+var funcao = () => {
+    console.log( 'sou uma arrow function')
+}
+funcao();
+
+//craindo uma calculadora = para este exercicio va,os utilizar alguns metodos nativos do JavaScript : Number (), prompt(), Alert(), Template Strings - para usar strings junto com expressoes.
+
